aeaf34ef5d781db7739fdd25794fd2a64b2c63b2
Evndroo <evandro.slipjr@gmail.com>
2024-03-04 20:54:10 -0300

    feat: add features based on alura courses

---
+++ hello.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+const MONITORING_QUANTITY = 5
+const MONITORING_INTERVAL = 5 * time.Second
+
+func main() {
+	showStartMessage()
+
+	for {
+		showOptions()
+		option := scanOptions()
+		fmt.Println()
+
+		switch option {
+		case 1:
+			startMonitoring()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
+	}
+}
+
+func showStartMessage() {
+	name := "Evandro"
+	version := 1.1
+
+	fmt.Println("Hello, ", name)
+	fmt.Println("Program version: ", version)
+	fmt.Println("================================")
+}
+
+func showOptions() {
+	fmt.Println("Selecione uma opção para iniciar")
+	fmt.Println()
+	fmt.Println("1. Iniciar monitoramento")
+	fmt.Println("2. Ver logs")
+	fmt.Println("0. Sair")
+	fmt.Println()
+}
+
+func scanOptions() int {
+	var option int
+
+	fmt.Scan(&option)
+
+	return option
+}
+
+func startMonitoring() {
+	fmt.Println("Monitorando...")
+	fmt.Println()
+	sites, error := getFileSiteList()
+
+	if error != nil {
+		fmt.Println("")
+		fmt.Println("Erro ao tentar ler arquivo de sites:")
+		os.Exit(-1)
+	}
+
+	for i := 0; i < MONITORING_QUANTITY; i++ {
+		for _, site := range sites {
+			testaSite(site)
+		}
+
+		time.Sleep(MONITORING_INTERVAL)
+		fmt.Println()
+		fmt.Println()
+	}
+}
+
+func testaSite(site string) {
+	result, err := http.Get(site)
+
+	if err != nil || result.StatusCode < 200 || result.StatusCode > 299 {
+		fmt.Println("O site está fora do ar!")
+		return
+	}
+
+	fmt.Println("O site ", site, " está online")
+}
+
+func getFileSiteList() ([]string, error) {
+	file, error := os.ReadFile("sites.txt")
+
+	if error != nil || file == nil {
+		fmt.Println(error)
+		return nil, error
+	}
+
+	return strings.Split(string(file), "\n"), nil
+}
